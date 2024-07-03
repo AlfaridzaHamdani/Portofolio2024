74df7bbc2f70596fa06dfcf0237f4f73c83f83e5
AlfaridzaHamdani <alfaridzah@gmail.com>
2024-07-03 09:19:55 +0700

    Fix: Export

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = fileURLToPath(import.meta.url);
 
 const nextConfig = {
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
